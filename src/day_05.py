432859c3a2f643c487dc98f8c1d4b1c1beeb18a0
diegoglozano <dgl_1096@hotmail.com>
2025-12-06 20:44:16 +0100

    [feat] day 5 part II

--- src/day_05.py
+++ src/day_05.py
@@ -1,5 +1,5 @@
 import os
-import math
+import itertools as it
 
 with open(f"data/day_05{os.getenv('DEV', '')}.txt") as f:
     data = f.read().splitlines()
@@ -24,26 +24,62 @@ for available_ingredient in available_ingredients:
 print(total)
 
 # Part II
-total = 0
-for idx, fresh_range in enumerate(ingredients, start=1):
-    print(f"{idx}/{len(ingredients)}")
-    min_fresh, max_fresh = fresh_range.split("-")
-    min_fresh = int(min_fresh)
-    max_fresh = int(max_fresh)
-    total += len(range(min_fresh, max_fresh + 1))
+def ranges_union(
+    range_base,
+    range_to_check,
+):
+    if range_to_check[0] not in range(range_base[0], range_base[1] + 1) and range_to_check[1] not in range(range_base[0], range_base[1] + 1):
+        return None
+    elif range_to_check[0] in range(range_base[0], range_base[1] + 1) and range_to_check[1] in range(range_base[0], range_base[1] + 1):
+        return range_base
+    elif range_to_check[0] in range(range_base[0], range_base[1] + 1) and range_to_check[1] > range_base[1]:
+        return (range_base[0], range_to_check[1])
+    elif range_to_check[0] < range_base[0] and range_to_check[1] in range(range_base[0], range_base[1] + 1):
+        return (range_to_check[0], range_base[1])
+    elif range_to_check[0] < range_base[0] and range_to_check[1] > range_base[1]:
+        return None
+    else:
+        return None
 
-print(total)
 
+ranges = [
+    tuple(map(int, block.split("-")))
+    for block in ingredients
+]
+ranges = sorted(
+    ranges,
+    key=lambda x: x[0],
+)
 
-ranges_to_check = []
-for idx, fresh_range in enumerate(ingredients, start=1):
-    print(f"{idx}/{len(ingredients)}")
-    min_fresh, max_fresh = fresh_range.split("-")
-    min_fresh = int(min_fresh)
-    max_fresh = int(max_fresh)
-    for range_to_check in ranges_to_check:
-
-        ranges_to_check.append(
-            (min_fresh, max_fresh),
+changed = True
+for i in it.count(1):
+    print(f"Iteration: {i}")
+    print(f"{ranges=}")
+    changed = False
+    combinations = (
+        it
+        .combinations(
+            ranges,
+            r=2,
         )
-print(ranges_to_check)
+    )
+    for combination in combinations:
+        union = ranges_union(
+            combination[0],
+            combination[1],
+        )
+        if union is not None:
+            changed = True
+            ranges.remove(combination[0])
+            ranges.remove(combination[1])
+            ranges.append(union)
+            break
+    if not changed:
+        print("Converged")
+        break
+print(
+    sum(
+        len(range(block[0], block[1] + 1))
+        for block in ranges
+    )
+)
