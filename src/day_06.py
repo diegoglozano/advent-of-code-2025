ebbbae5bf44abd76c2fb2dc26dae90031134a26c
diegoglozano <dgl_1096@hotmail.com>
2025-12-06 17:31:23 +0100

    [feat] day 6 part II

--- src/day_06.py
+++ src/day_06.py
@@ -1,5 +1,6 @@
 import os
 import re
+import itertools as it
 
 from pprint import pprint
 from functools import reduce
@@ -8,7 +9,7 @@ with open(f"data/day_06{os.getenv('DEV', '')}.txt") as f:
     data = f.read().splitlines()
 
 data = [
-    re.sub("\s+", " ", line.strip()).split(" ")
+    re.sub(r"\s+", " ", line.strip()).split(" ")
     for line in data # [::-1]
 ]
 
@@ -28,4 +29,49 @@ for row in data:
 
 print(sum(results))
 
-pprint(data)
+with open(f"data/day_06{os.getenv('DEV', '')}.txt") as f:
+    data = f.read().splitlines()
+    data = it.zip_longest(*data, fillvalue="")
+
+OPERATORS = ("*", "+")
+current_operator = None
+total = []
+current_numbers = []
+for line in data:
+    if all([l == " " for l in line]):
+        if current_operator == "*":
+            block_result = reduce(
+                lambda x, y: x * y,
+                current_numbers
+            )
+        elif current_operator == "+":
+            block_result = reduce(
+                lambda x, y: x + y,
+                current_numbers
+            )
+        else:
+            raise Exception("Bug")
+        total.append(block_result)
+        current_numbers = []
+        continue
+    if line[-1] in OPERATORS:
+        current_operator = line[-1]
+    numbers = int("".join(line[:-1]))
+    current_numbers.append(numbers)
+
+# For the last block. Ugly
+if current_operator == "*":
+    block_result = reduce(
+        lambda x, y: x * y,
+        current_numbers
+    )
+elif current_operator == "+":
+    block_result = reduce(
+        lambda x, y: x + y,
+        current_numbers
+    )
+else:
+    raise Exception("Bug")
+total.append(block_result)
+
+print(sum(total))
